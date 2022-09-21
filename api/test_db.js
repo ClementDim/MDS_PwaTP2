f411cfc0d20139b506646986a53adcf30ef5920d
Clement Dimanchin <clementdimanchin@free.fr>
2022-09-21 21:38:10 +0200

    :bug: Fix DB connexion (adding wait-for script in docker-compose.yml)

--- api/test_db.js
+++ api/test_db.js
@@ -1,16 +1,18 @@
 const { Sequelize } = require('sequelize');
 //const sequelize = new Sequelize('mariadb://admin:admin@mariadb/db_tp2');
 
-const sequelize = new Sequelize('db_tp2', 'admin', 'admin', {
+const sequelize = new Sequelize('db_tp2', 'admin', 'password', {
   host: 'mariadb',
   dialect: 'mariadb'
 });
 
 (async () => {
+
+  //await new Promise((resolve, reject) => {setTimeout(resolve, 1000000)});
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.log('Unable to connect to the database:', error);
   }
 })();
